1640cf67d3baded3f2e7262b4532bc46252a8ced
Benjamin <benjamin.gavriely@gmail.com>
2024-01-31 17:42:52 -0500

    yy

--- adventure.py
+++ adventure.py
@@ -33,11 +33,28 @@ if __name__ == "__main__":
     while not p.victory:
         location = w.get_location(p.x, p.y)
 
+        # checks if the player has any moves left
+        if p.num_moves == 45:
+            print("You only have 5 moves left!")
+        elif p.num_moves == 46:
+            print("You only have 4 moves left!")
+        elif p.num_moves == 47:
+            print("You only have 3 moves left!")
+        elif p.num_moves == 48:
+            print("You only have 2 moves left!")
+        elif p.num_moves == 49:
+            print("You only have 1 move left!")
+        elif p.num_moves == 50:
+            print("You have no moves left :(")
+            print("Game over.")
+            break
+
         # print the long or short description
         if location.visited_before:
             print(location.brief_description)
         else:
             print(location.long_description)
+            p.score += location.points_for_visit
             location.visited_before = True
 
         print("What to do? \n")
@@ -58,8 +75,17 @@ if __name__ == "__main__":
         if choice == "inventory":
             for item in p.inventory:
                 print(item)
+            print("'Drop' + item to drop an item")
+            print("Back to return to actions")
+            choice = input()
+            if choice == "Back":
+                continue
 
-
+            # allow the user to drop items
+            for item in p.inventory:
+                if choice == "Drop" + item.name:
+                    p.inventory.remove(item)
+                    item.current_position = location.map_position
 
         if choice == "score":
             print(p.score)
@@ -73,36 +99,42 @@ if __name__ == "__main__":
         if choice == "Go East":
             # change the player's x coordinate
             p.x += 1
-
-
+            p.num_moves += 1
+            continue
 
         if choice == "Go West":
             # change the player's x coordinate
             p.x -= 1
+            p.num_moves += 1
+            continue
 
         if choice == "Go North":
             # change the player's y coordinates
             p.y -= 1
+            p.num_moves += 1
+            continue
 
         if choice == "Go South":
             # change the player's y coordinates
             p.y += 1
-        for item in w.items:
+            p.num_moves += 1
+            continue
 
+        for item in w.items:
             if choice == "Take" + item.name:
                 print("You picked up the " + item.name)
                 p.inventory.append(item)
-                p.score +=
+                item.current_location = 0
+                continue
+
         # win conditions
+        # if you reach the final destination with all of the items
+        if location == "int" and len(p.inventory) != len(w.items): #TODO put the number of the final location
+            print("You do not have all of the items, so you cannot write the exam!")
+        elif location == "int": #TODO put the number of the final location
+            print("Congratulations! You have collected all of the items and are ready to write the final exam!")
+            print("You forgot to study and barely passed the course. Your career prospects dwindle and"
+                  "you become an unsuccessful rapper. At least you passed")
+            p.victory = True
 
 
-        # TODO: CALL A FUNCTION HERE TO HANDLE WHAT HAPPENS UPON THE PLAYER'S CHOICE
-        #  REMEMBER: the location = w.get_location(p.x, p.y) at the top of this loop will update the location if
-        #  the choice the player made was just a movement, so only updating player's position is enough to change the
-        #  location to the next appropriate location
-        #  Possibilities:
-        #  A helper function such as do_action(w, p, location, choice)
-        #  OR A method in World class w.do_action(p, location, choice)
-        #  OR Check what type of action it is, then modify only player or location accordingly
-        #  OR Method in Player class for move or updating inventory
-        #  OR Method in Location class for updating location item info, or other location data etc....
