001f5c2a6ef2cf5e7b022447e67d3f4be357f701
Benjamin <benjamin.gavriely@gmail.com>
2024-01-31 17:15:08 -0500

    yy

--- adventure.py
+++ adventure.py
@@ -59,6 +59,8 @@ if __name__ == "__main__":
             for item in p.inventory:
                 print(item)
 
+
+
         if choice == "score":
             print(p.score)
 
@@ -67,6 +69,33 @@ if __name__ == "__main__":
 
         if choice == "back":
             continue
+
+        if choice == "Go East":
+            # change the player's x coordinate
+            p.x += 1
+
+
+
+        if choice == "Go West":
+            # change the player's x coordinate
+            p.x -= 1
+
+        if choice == "Go North":
+            # change the player's y coordinates
+            p.y -= 1
+
+        if choice == "Go South":
+            # change the player's y coordinates
+            p.y += 1
+        for item in w.items:
+
+            if choice == "Take" + item.name:
+                print("You picked up the " + item.name)
+                p.inventory.append(item)
+                p.score +=
+        # win conditions
+
+
         # TODO: CALL A FUNCTION HERE TO HANDLE WHAT HAPPENS UPON THE PLAYER'S CHOICE
         #  REMEMBER: the location = w.get_location(p.x, p.y) at the top of this loop will update the location if
         #  the choice the player made was just a movement, so only updating player's position is enough to change the
